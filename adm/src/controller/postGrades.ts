4a82c5773a2f9e837d168e32d57bf7805a9bd60b
devTiconnecte <85088970+TonySandro@users.noreply.github.com>
2022-01-26 20:57:09 -0300

    create new student and your grades

--- adm/src/controller/postGrades.ts
+++ adm/src/controller/postGrades.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from "express";
-import { dbFake } from "../repository/dbFake";
+import { gradesModel } from "../database/models/gradesModel";
+import { studentModel } from "../database/models/studentModel";
 
 export class PostGrades {
     async insert(req: Request, res: Response) {
         try {
-            const { student, grade } = req.body
+            const { student, grades } = req.body
+            const student2 = await studentModel.create({
+                name: student
+            })
+            const studentId = JSON.parse(JSON.stringify(student2))
 
-            dbFake.push(req.body)
-            return res.send(dbFake)
+            const grade2 = await gradesModel.create({
+                n1: grades.n1,
+                n2: grades.n2,
+                n3: grades.n3,
+                n4: grades.n4,
+                idStudent: studentId.id
+            })
+
+            return res.status(201).json({ student2, grade2 })
         } catch (err) {
             return res.send(err)
         }
